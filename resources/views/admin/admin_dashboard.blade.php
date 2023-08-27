eaec4161e101647aeec1390044034abb25230a3e
pocari1210 <pocari.design@gmail.com>
2023-08-27 22:48:54 +0900

    tinymce導入

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -1,4 +1,4 @@
-!doctype html>
+<!doctype html>
 <html lang="en">
 
 <head>
@@ -129,6 +129,17 @@
     });
   </script>
   <!--datatable JS-->
+
+  <!--tinymce JS-->
+  <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+  <script>
+    tinymce.init({
+      selector: 'textarea#myeditorinstance', // Replace this CSS selector to match the placeholder element for TinyMCE
+      plugins: 'powerpaste advcode table lists checklist',
+      toolbar: 'undo redo | blocks| bold italic | bullist numlist checklist | code | table'
+    });
+  </script>
+  <!--tinymce JS-->
 
 </body>
 
