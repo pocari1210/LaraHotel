ce4b4718815a9d311d679ee28e1bdb5a82f821c9
pocari1210 <pocari.design@gmail.com>
2023-08-27 09:11:26 +0900

    Teamの追加ページ遷移と保存処理

--- app/Http/Controllers/Backend/TeamController.php
+++ app/Http/Controllers/Backend/TeamController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use InterventionImage;
+use Carbon\Carbon;
 use App\Models\Team;
 
 class TeamController extends Controller
@@ -16,5 +18,34 @@ class TeamController extends Controller
       'backend.team.all_team',
       compact('team')
     );
+  } // End Method 
+
+  public function AddTeam()
+  {
+    return view('backend.team.add_team');
+  } // End Method 
+
+  public function StoreTeam(Request $request)
+  {
+    $image = $request->file('image');
+    $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+    InterventionImage::make($image)->resize(550, 670)->save('storage/upload/team/' . $name_gen);
+    $save_url = 'storage/upload/team/' . $name_gen;
+
+    Team::insert([
+
+      'name' => $request->name,
+      'postion' => $request->postion,
+      'facebook' => $request->facebook,
+      'image' => $save_url,
+      'created_at' => Carbon::now(),
+    ]);
+
+    $notification = array(
+      'message' => 'Team Data Inserted Successfully',
+      'alert-type' => 'success'
+    );
+
+    return redirect()->route('all.team')->with($notification);
   } // End Method 
 }
--- routes/web.php
+++ routes/web.php
@@ -93,5 +93,11 @@ Route::middleware(['auth', 'roles:admin'])->group(function () {
 
     Route::get('/all/team', 'AllTeam')
       ->name('all.team');
+
+    Route::get('/add/team', 'AddTeam')
+      ->name('add.team');
+
+    Route::post('/team/store', 'StoreTeam')
+      ->name('team.store');
   });
 }); // End Admin Group Middleware 
