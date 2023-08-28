dada79b23e1f13cf2eeaa50197096e40a4a36bb9
pocari1210 <pocari.design@gmail.com>
2023-08-28 23:13:12 +0900

    Facility疎通(DOM)

--- app/Http/Controllers/Backend/RoomController.php
+++ app/Http/Controllers/Backend/RoomController.php
@@ -7,16 +7,18 @@ use Illuminate\Http\Request;
 use Intervention;
 use Carbon\Carbon;
 use App\Models\Room;
+use App\Models\Facility;
 
 class RoomController extends Controller
 {
   public function EditRoom($id)
   {
     $editData = Room::find($id);
+    $basic_facility = Facility::where('rooms_id', $id)->get();
 
     return view(
       'backend.allroom.rooms.edit_rooms',
-      compact('editData')
+      compact('editData', 'basic_facility')
     );
   } //End Method 
 }
