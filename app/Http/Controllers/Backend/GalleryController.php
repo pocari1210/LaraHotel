4640290b5622977a9280715556fde24cb58f412d
pocari1210 <pocari.design@gmail.com>
2023-09-11 00:18:44 +0900

    SiteGalleryの編集ページ遷移,更新処理,削除処理

--- app/Http/Controllers/Backend/GalleryController.php
+++ app/Http/Controllers/Backend/GalleryController.php
@@ -49,4 +49,51 @@ class GalleryController extends Controller
 
     return redirect()->route('all.gallery')->with($notification);
   } // End Method 
+
+  public function EditGallery($id)
+  {
+
+    $gallery = Gallery::find($id);
+    return view('backend.gallery.edit_gallery', compact('gallery'));
+  } // End Method 
+
+  public function UpdateGallery(Request $request)
+  {
+
+    $gal_id = $request->id;
+    $img = $request->file('photo_name');
+
+    $name_gen = hexdec(uniqid()) . '.' . $img->getClientOriginalExtension();
+    InterventionImage::make($img)->resize(550, 550)->save('storage/upload/gallery/' . $name_gen);
+    $save_url = 'storage/upload/gallery/' . $name_gen;
+
+    Gallery::find($gal_id)->update([
+      'photo_name' => $save_url,
+    ]);
+
+    $notification = array(
+      'message' => 'Gallery Updated Successfully',
+      'alert-type' => 'success'
+    );
+
+    return redirect()->route('all.gallery')->with($notification);
+  } // End Method 
+
+  public function DeleteGallery($id)
+  {
+
+    $item = Gallery::findOrFail($id);
+
+    $img = $item->photo_name;
+    unlink($img);
+
+    Gallery::findOrFail($id)->delete();
+
+    $notification = array(
+      'message' => 'Gallery Image Deleted Successfully',
+      'alert-type' => 'success'
+    );
+
+    return redirect()->back()->with($notification);
+  }   // End Method 
 }
--- routes/web.php
+++ routes/web.php
@@ -301,6 +301,15 @@ Route::middleware(['auth', 'roles:admin'])->group(function () {
 
     Route::post('/store/gallery', 'StoreGallery')
       ->name('store.gallery');
+
+    Route::get('/edit/gallery/{id}', 'EditGallery')
+      ->name('edit.gallery');
+
+    Route::post('/update/gallery', 'UpdateGallery')
+      ->name('update.gallery');
+
+    Route::get('/delete/gallery/{id}', 'DeleteGallery')
+      ->name('delete.gallery');
   });
 }); // End Admin Group Middleware 
 
