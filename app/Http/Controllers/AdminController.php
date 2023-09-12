a94b917e9282bbb3ef6bec3a67692592548d2c29
pocari1210 <pocari.design@gmail.com>
2023-09-13 00:30:42 +0900

    UpdateAdminDashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -18,7 +18,7 @@ class AdminController extends Controller
 
   public function AdminDashboard()
   {
-    return view('admin.admin_dashboard');
+    return view('admin.index');
   } // End Method 
 
   // ★プロフィールページ疎通のコントローラー★
